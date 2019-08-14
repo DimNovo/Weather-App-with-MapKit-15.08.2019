5c4b93440a23710e6abc725d4dc93f589648cc15
Dmitry Novosyolov <i1710@icloud.com>
2019-08-15 00:16:36 +0300

    Initial Commit

---
+++ Weather App with MapKit/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Weather App with MapKit
+//
+//  Created by Dmitry Novosyolov on 15/08/2019.
+//  Copyright © 2019 Dmitry Novosyolov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
